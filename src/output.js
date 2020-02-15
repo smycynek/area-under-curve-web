2201dbed68a42226bc7d6d2875f046910a18d8c2
Steven Mycynek <smycynek@buildingengines.com>
2020-02-15 11:02:59 -0500

    refactor

--- src/output.js
+++ src/output.js
@@ -4,41 +4,83 @@ function Plus(props) {
   return "+"
 }
 
-function Cubic({ coefficient }) {
-  if (coefficient === 0) {
-    return <React.Fragment />
+function Term({ power, coefficient = null }) {
+  if (power === 0) {
+    return <React.Fragment> {coefficient}</React.Fragment>
   }
-  if (coefficient === 1) {
-    return (<React.Fragment>x <sup> 3 </sup>  </React.Fragment>);
+  else if (power === 1) {
+    return <React.Fragment> {coefficient}x</React.Fragment>
   }
-  return (<React.Fragment>{coefficient} x <sup> 3 </sup>  </React.Fragment>);
+  else {
+    return <React.Fragment> {coefficient}x<sup>{power}</sup></React.Fragment>
+  }
 }
 
-function Quadratic({ coefficient }) {
+function CubicTerm({ coefficient }) {
   if (coefficient === 0) {
-    return <React.Fragment />
+    return null
   }
   if (coefficient === 1) {
-    return (<React.Fragment>x <sup> 2 </sup>  </React.Fragment>);
+    return (<Term power={3} />);
   }
-  return (<React.Fragment>{coefficient} x <sup> 2 </sup>  </React.Fragment>);
+
+  return (<Term power={3} coefficient={coefficient} />)
 }
 
-function Linear({ coefficient }) {
+function QuadraticTerm({ coefficient }) {
   if (coefficient === 0) {
-    return <React.Fragment />
+    return null
   }
   if (coefficient === 1) {
-    return (<React.Fragment>x  </React.Fragment>);
+    return (<Term power={2} />);
   }
-  return (<React.Fragment>{coefficient} x </React.Fragment>);
+  return (<Term power={2} coefficient={coefficient} />);
 }
 
-function Constant({ coefficient }) {
+function LinearTerm({ coefficient }) {
+  if (coefficient === 0) {
+    return null
+  }
+  if (coefficient === 1) {
+    return (<Term power={1} />);
+  }
+  return (<Term power={1} coefficient={coefficient} />);
+}
+
+function ConstantTerm({ coefficient }) {
   if (coefficient === 0) {
     return <React.Fragment />
   }
-  return (<React.Fragment> {coefficient} </React.Fragment>);
+  return (<Term power={0} coefficient={coefficient} />);
+}
+
+function needsConcatination(power, data) {
+  //if (data.get(power-1) < 0)
+  //  return false
+  switch (power) {
+    case 3:
+      return data.get(3) && data.get(2)
+    case 2:
+      return (data.get(1) && (data.get(3) || data.get(2)))
+    case 1:
+      return (data.get(0) && (data.get(3) || data.get(2) || data.get(1)))
+    case 0:
+      return false
+    default:
+      return false
+  }
+}
+
+function Polynomial({ data }) {
+  return (
+    <React.Fragment>
+      <span>f(x) = </span>
+      <CubicTerm coefficient={data.get(3)} /> {needsConcatination(3, data) ? <Plus /> : null}
+      <QuadraticTerm coefficient={data.get(2)} /> {needsConcatination(2, data) ? <Plus /> : null}
+      <LinearTerm coefficient={data.get(1)} /> {needsConcatination(1, data) ? <Plus /> : null}
+      <ConstantTerm coefficient={data.get(0)} />
+    </React.Fragment>
+  )
 }
 
 function Output({ data }) {
@@ -46,11 +88,10 @@ function Output({ data }) {
     <div>
       <ul>
         <li>
-          Polynomial:
-          f(x) = <Cubic coefficient={data.polynomial.get(3)} /> {(data.polynomial.get(3) && data.polynomial.get(2)) ? <Plus /> : ""}
-          <Quadratic coefficient={data.polynomial.get(2)} />  {((data.polynomial.get(2) || data.polynomial.get(3)) && data.polynomial.get(1)) ? <Plus /> : ""}
-          <Linear coefficient={data.polynomial.get(1)} />     {((data.polynomial.get(1) || data.polynomial.get(2) || data.polynomial.get(3)) && data.polynomial.get(0)) ? <Plus /> : ""}
-          <Constant coefficient={data.polynomial.get(0)} />
+          <React.Fragment>
+            <span>Polynomial: </span>
+            <Polynomial data={data.polynomial} />
+          </React.Fragment>
         </li>
         <li>
           Bounds:
